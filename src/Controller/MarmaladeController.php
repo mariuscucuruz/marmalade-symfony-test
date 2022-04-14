efefb070006081634bf999654f9444b42e559a60
Marius Cucuruz <marius.cucuruz@gmail.com>
2022-04-14 19:33:52 +0100

    route scaffolding

---
+++ src/Controller/MarmaladeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class MarmaladeController extends AbstractController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function resolveRegistrationFromRequest(Request $request): JsonResponse
+    {
+        return $this->json($request->toArray());
+    }
+}
